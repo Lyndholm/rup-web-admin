9dc70b05e706fbf871b6aac509fb04655d2ab55b
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2024-10-26 15:04:40 +0300

    Switch to postgresql

--- app/core/settings/default.py
+++ app/core/settings/default.py
@@ -74,7 +74,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DATABASE_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
+        'NAME': os.getenv('POSTGRES_DB', BASE_DIR / 'db.sqlite3'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'password'),
+        'ATOMIC_REQUESTS': True,
     }
 }
 
